bb90cc37e9ff851d532b167415f7be4d202f82b8
Adriana Borges <drika.fonseca.borges@gmail.com>
2022-03-21 18:20:47 -0300

    + Configuração do AutoMapper;

---
+++ Kasznar.Application/AutoMapper/AutoMapperSetup.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Kasznar.Application.ViewModels;
+using Kasznar.Domain.Entities;
+
+namespace Kasznar.Application.AutoMapper
+{
+    public class AutoMapperSetup : Profile
+    {
+        public AutoMapperSetup()
+        {
+            #region ViewwModelToDomain
+
+            CreateMap<UserViewModel, DebuggerNonUserCodeAttribute>();
+
+            #endregion
+
+            #region DomainToViewModel
+
+            CreateMap<User, UserViewModel>();
+
+            #endregion
+        }
+    }
+}
