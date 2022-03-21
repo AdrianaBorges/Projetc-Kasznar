39663a7fab5ad393cd04ab46cf31d358df467459
Adriana Borges <drika.fonseca.borges@gmail.com>
2022-03-21 16:12:33 -0300

    + Implementando api GET para retorno de valores do banco de dados;

--- Kasznar.Application/Services/UserService.cs
+++ Kasznar.Application/Services/UserService.cs
@@ -5,14 +5,30 @@ using System.Text;
 using System.Threading.Tasks;
 using Kasznar.Application.Interfaces;
 using Kasznar.Application.ViewModels;
+using Kasznar.Domain.Entities;
+using Kasznar.Domain.Interfaces;
 
 namespace Kasznar.Application.Services
 {
-    public class UserService: IUserService 
+    public class UserService: IUserService
     {
+        private readonly IUserRepository userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+
         public List<UserViewModel> Get()
         {
-            return null;
+            List<UserViewModel> _userViewModels = new List<UserViewModel>();
+
+            IEnumerable<User> _users = this.userRepository.GetAll();
+
+            foreach (var item in _users)
+                _userViewModels.Add(new UserViewModel() { Id = item.Id, Email = item.Email, Name = item.Name });
+            
+            return _userViewModels;
         }
     }
 }
--- Kasznar.IoC/NativeInjector.cs
+++ Kasznar.IoC/NativeInjector.cs
@@ -1,16 +1,29 @@
 ﻿using Kasznar.Application.Interfaces;
 using Kasznar.Application.Services;
+using Kasznar.Data.Repositories;
+using Kasznar.Domain.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
 
 // Injeção de dependência e inversão de controles
 namespace Kasznar.IoC
 {
-    public class NativeInjector
+    public static class NativeInjector
     {
         // Metodo responsável pela injeção de dependência
         public static void RegisterServices(IServiceCollection services)
         {
+            #region  Services
+
             services.AddScoped<IUserService, UserService>();
+
+            #endregion
+
+            #region Repositories
+
+            services.AddScoped<IUserRepository, UserRepository>();
+            
+            #endregion
+
 
         }
     }
