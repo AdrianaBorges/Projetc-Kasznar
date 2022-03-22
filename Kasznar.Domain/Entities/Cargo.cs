4bad2272a08fe90ecf0a44363260bd33b6089552
Adriana Borges <drika.fonseca.borges@gmail.com>
2022-03-22 20:50:27 -0300

    + Criação de entidades;

---
+++ Kasznar.Domain/Entities/Cargo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kasznar.Domain.Entities
+{
+    public class Cargo
+    {
+        public int Codigo { get; set; }
+        public string Descricao { get; set; }
+    }
+}
---
+++ Kasznar.Domain/Interfaces/ICargoRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kasznar.Domain.Entities;
+
+namespace Kasznar.Domain.Interfaces
+{
+    public interface ICargoRepository: IRepository<Cargo>
+    {
+        IEnumerable<Cargo> GetAll();
+
+    }
+}
